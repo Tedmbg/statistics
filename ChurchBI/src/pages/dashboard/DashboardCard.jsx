6047611d7391f9cb24d460bb5c614100088d4889
alphaxardjr <codemaster189@gmail.com>
2024-11-08 00:46:35 +0300

    add dashboard api

--- ChurchBI/src/pages/dashboard/DashboardCard.jsx
+++ ChurchBI/src/pages/dashboard/DashboardCard.jsx
@@ -5,60 +5,96 @@ import {useDashboardData} from './dashboardData';
 
 
 function DashboardCard() {
-  console.log(useDashboardData().members.total_members)
+  const { members, conversions, ministries, baptisms, discipleshipClasses, staff } = useDashboardData();
+  const datas=[
+    {
+      "id": 1,
+      "icon": "/assets/congregation.png",
+      "title": "Total Number Of Members",
+      "value":members?.total_members ||0
+    },
+    {
+      "id": 2,
+      "icon": "/assets/conversions.png",
+      "title": "Total Number Of Conversions",
+      "value": conversions?.total_conversions ||0
+    },
+    {
+      "id": 3,
+      "icon": "/assets/ministry.png",
+      "title": "Total Number Of Ministries",
+      "value": ministries?.total_ministries ||0
+    },
+    {
+      "id": 4,
+      "icon": "/assets/baptism.png",
+      "title": "Total Number of Baptisms",
+      "value": baptisms?.total_baptisms ||0
+    },
+    {
+      "id": 5,
+      "icon": "/assets/dicsipleship.png",
+      "title": "Discipleship Classes Completed",
+      "value": discipleshipClasses?.completed_classes ||0
+    },
+    {
+      "id": 6,
+      "icon": "/assets/staff.png",
+      "title": "Church Staff",
+      "value": staff?.total_staff||0
+    }
+  ]
   return (
-    <div>hey</div>
-    // <Box
-    //   sx={{
-    //     display:'flex',
-    //     flexDirection:'row',
-    //     gap:9,
-    //     mt:6,
-    //     ml:6,
-    //     flexWrap: 'wrap',
-    //     '& > :not(style)': {
-    //       m: 1,
-    //       maxWidth: 390,
-    //       padding: "2em 2em",
-    //       borderRadius: "2rem",
-    //       cursor: "pointer",
-    //       userSelect: "none",
-    //       backgroundColor: "#FFFFFF",
-    //     },
-    //   }}
-    // >
-    //   {dashboardData.map((item) => (
-    //     <Paper
-    //       key={item.id}
-    //       sx={{
-    //         display: 'flex',
-    //         alignItems: 'center',
-    //         justifyContent: 'center',
-    //         gap: '2em',
-    //         margin: "auto",
-    //         width:'20em',
-    //       }}
-    //     >
-    //       <img
-    //         src={item.icon} // Use icon path from JSON data
-    //         style={{ width: 70, height: 70 ,objectFit:"contain"}}
-    //       />
-    //       <Box>
-    //         <Typography
-    //           variant="h3"
-    //           color="black"
-    //         >
-    //           {item.value}
-    //         </Typography>
-    //         <Typography
-    //           variant="subtitle"
-    //         >
-    //           {item.title}
-    //         </Typography>
-    //       </Box>
-    //     </Paper>
-    //   ))}
-    // </Box>
+    <Box
+      sx={{
+        display:'flex',
+        flexDirection:'row',
+        gap:9,
+        mt:6,
+        ml:6,
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 1,
+          maxWidth: 390,
+          padding: "2em 2em",
+          borderRadius: "2rem",
+          cursor: "pointer",
+          userSelect: "none",
+          backgroundColor: "#FFFFFF",
+        },
+      }}
+    >{datas.map((item) => (
+        <Paper
+          key={item.id}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '2em',
+            margin: "auto",
+            width:'20em',
+          }}
+        >
+          <img
+            src={item.icon} // Use icon path from JSON data
+            style={{ width: 70, height: 70 ,objectFit:"contain"}}
+          />
+          <Box>
+            <Typography
+              variant="h3"
+              color="black"
+            >
+              {item.value}
+            </Typography>
+            <Typography
+              variant="subtitle"
+            >
+              {item.title}
+            </Typography>
+          </Box>
+        </Paper>
+      ))}
+    </Box>
   );
 }
 
