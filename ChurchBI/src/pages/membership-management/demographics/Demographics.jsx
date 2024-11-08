760a2e8721034aa31e78fd0e75ce84bfb4925198
alphaxardjr <codemaster189@gmail.com>
2024-11-09 02:45:04 +0300

    add demographic api

--- ChurchBI/src/pages/membership-management/demographics/Demographics.jsx
+++ ChurchBI/src/pages/membership-management/demographics/Demographics.jsx
@@ -1,68 +1,79 @@
 import { Box,Typography } from "@mui/material"
-import { sampleData } from "./demodata"
 import DemographicCard from "./DemographicCard"
 import DoughnutC from "../../../components/DoughnutC"
 import LineChart from "../../../components/LineChart"
 import StackedBar from "./StackedBar"
+import { useState, useEffect } from "react"
 
-export default function Demographics() {
-  return (
-    <div className="demographic-main-container">
+
+// datus
+import { fetchDemographicData } from "./demographicData"
+
+const styles={
+  flexGrow:"1",
+  height:"20rem",
+}
+
+export default function Demographics() { 
+  const [demographicData, setDemographicData] = useState(null)
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetchDemographicData();
+      setDemographicData(data);
+    };
+    getData();
+  }, []);
+
+  if (!demographicData) {
+    return <div>Loading...</div>;
+  }
+
+
+ 
+ return (
+   
+   <div className="demographic-main-container">
       <Typography
        variant="h4"
        sx={{
-        padding:'1rem 0'
-       }}
-       >Overview</Typography>
+         padding:'1rem 0'
+        }}
+        >Overview</Typography>
       <div className="demographic-container">
         <div className="left-demo-content">
         
-        <div style={{
-          display:"flex",
-          flexWrap:"wrap",
-          gap:"1em",
-          border:"1px solid blue"
-        }}>
-          <Box sx={{width:"15rem"}}>
-            <DemographicCard data={sampleData.locationDistribution} title="Data"/>
+            <div style={{
+              display:"flex",
+              flexWrap:"wrap",
+              justifyContent:"space-between",
+              gap:"1em",
+            }}>
+              <Box sx={styles}>
+                <DemographicCard data={demographicData.ageDistribution} title="Age"/>
+              </Box>
+
+              <Box sx={styles}>
+                <DemographicCard data={demographicData.genderDistribution} title="Gender"/>
+              </Box>
+              <Box sx={styles}>
+                <DemographicCard data={demographicData.marriage} title="Marriage"/>
+              </Box>
+              </div>
+
+          <Box >
+            <LineChart data={demographicData.membershipOverTime}/>
+          </Box>
+
+          <Box >
+            <StackedBar data={demographicData.locationDistribution} title="Residence"/>
           </Box>
           
-          <Box sx={{width:"15rem"}}>
-            <DemographicCard data={sampleData.locationDistribution} title="Data"/>
+          <Box>
+            <StackedBar data={demographicData.countryOfOrigin} title="County of Origin"/>
           </Box>
 
-          </div>
+      
 
-          <Box style={{maxWidth:"31rem"}}>
-            <LineChart data={sampleData.membershipOverTime}/>
-          </Box>
-
-          <div style={{
-          display:"flex",
-          flexWrap:"wrap",
-          gap:"1em",
-          border:"1px solid blue"
-        }}>
-          <Box sx={{width:"15rem"}}>
-            <DemographicCard data={sampleData.locationDistribution} title="Data"/>
-          </Box>
-          
-          <Box sx={{width:"15rem"}}>
-            <DemographicCard data={sampleData.locationDistribution} title="Data"/>
-          </Box>
-
-          </div>
-
-          
-        </div>
-
-        <div className="right-demo-content">
-         <Box>
-         <DemographicCard data={sampleData.ageDistribution} title="Data"/>
-         </Box>
-         <Box>
-         <DemographicCard data={sampleData.ageDistribution} title="Data"/>
-         </Box>
           
         </div>
       </div>
--- ChurchBI/src/pages/membership-management/demographics/StackedBar.jsx
+++ ChurchBI/src/pages/membership-management/demographics/StackedBar.jsx
@@ -1,21 +1,39 @@
-import Barchart from "../../../components/BarChart"
-import { sampleData } from "./demodata"
-function StackedBar() {
-    const options ={
-        indexAxis: 'y',
-        datalabels: {
-            anchor: 'end',
-            align: 'right',
-            color: 'white',
-            font: {
-              weight: 'bold',
-              size: 14,
-            },}
-    }
+import { Bar } from "react-chartjs-2";
+function StackedBar({data,title}) {
+    const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false,
+          },
+          title: {
+            display: true,
+            text:title,
+            align:"start",
+            position:"top",
+            color:"#000000",
+            padding:20,
+            font:{
+              size:23,
+              family:"Inter"
+            }
+        }
+        },
+        scales: {
+          x: {
+            beginAtZero: true,
+          },
+          y: {
+            beginAtZero: true,
+          },
+        },
+      };
   return (
-    <div>
-      <Barchart data={sampleData.ageDistribution} options={options}/>
-    </div>
+    <Bar style={{
+        padding:"2em 1em",
+        background:"#fff",
+        borderRadius:".5rem"
+      }} data={data} options={options} />
   )
 }
 
